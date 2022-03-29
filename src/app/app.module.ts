cc9893b04a2bff844cd65b6a26ac9474cf4d1474
sky406 <60670435+sky406@users.noreply.github.com>
2022-03-29 13:10:42 -0700

    removed star checks and adjusted the style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
-import { CheckedstarComponent } from './checkedstar/checkedstar.component';
-import { UncheckedstarComponent } from './uncheckedstar/uncheckedstar.component';
+// import { CheckedstarComponent } from './checkedstar/checkedstar.component';
+// import { UncheckedstarComponent } from './uncheckedstar/uncheckedstar.component';
 import { LikebuttonComponent } from './likebutton/likebutton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    CheckedstarComponent,
-    UncheckedstarComponent,
+    // CheckedstarComponent,
+    // UncheckedstarComponent,
     LikebuttonComponent
   ],
   imports: [

1c185460d715d4854d80ca00959600c98697dae6
sky406 <60670435+sky406@users.noreply.github.com>
2022-03-29 17:48:38 -0700

    changed some attributes

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -14,7 +14,8 @@ export class PostsComponent implements OnInit {
       image:"",
       date:"12-12-12",
       userimg:"",
-      isstared:false
+      isstared:false,
+      id:0
     },
     {
       user:"the amongus user",
@@ -22,7 +23,8 @@ export class PostsComponent implements OnInit {
       image:"https://pioneeroptimist.com/wp-content/uploads/2021/03/among-us-6008615_1920-838x900.png",
       date:"4-20-20",
       userimg:"https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec",
-      isstared:false
+      isstared:false,
+      id:1
     },
     {
       user:"person of the internet",
@@ -30,7 +32,17 @@ export class PostsComponent implements OnInit {
       image:"",
       date:"12-12-12",
       userimg:'',
-      isstared:false
+      isstared:false,
+      id:2
+    },
+    {
+      user:'gigachad',
+      userimg:'https://i.kym-cdn.com/photos/images/facebook/001/896/232/2a0.jpg',
+      caption:'hello',
+      image:'https://pbs.twimg.com/media/EpfCgqPXEAgjLAW?format=jpg&name=360x360',
+      date:'1-12-1',
+      isstared:false,
+      id:3
     }
   ]
 
